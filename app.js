1ac1fc2563d935eacfb1ed26d30939744f6d0ea6
Daniel Ramirez <danielramirezduque1@gmail.com>
2024-09-13 15:18:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const app = express()
+const movies = require('./movies.json')
+
+//Get the value this PORT in enviroment variables or usage PORT 3000
+const PORT = process.env.PORT || 3000
+
+app.use(express.json()) // Para manejar JSON en el cuerpo de la solicitud
+
+app.get('/movies', (req, res) => {
+  // JSON.stringify is used to convert an object or array into a JSON string that can be sent in the HTTP response.
+  // Get all the movies
+  const data = JSON.stringify(movies)
+  res.status(200).send(data)
+})
+
+//Obtener movie por id
+app.get('/movies/:id', (req, res) => {
+  const movie = movies.find((movie) => movie.id == req.params.id)
+  if (!movie) return res.status(404).send('Libro no encontrado')
+  res.json(movie)
+})
+
+//Create a new movie
+app.post('/movies', (req, res) => {
+  const newMovie = {
+    id: movies.length + 1,
+    title: req.body.title,
+    year: req.body.year,
+    director: req.body.director,
+    duration: req.body.duration,
+    poster: req.body.poster,
+    genre: req.body.genre,
+    rate: req.body.rate ? req.body.rate === '' : 7.8
+  }
+  movies.push(newMovie)
+  res.status(200).send(newMovie)
+})
+
+//Update a movie
+
+//Delete movie
+
+app.listen(PORT, () => {
+  console.log(`App listening on port http://localhost:${PORT}`)
+})
